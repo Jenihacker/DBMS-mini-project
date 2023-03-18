a38d1caad1f02b9834c0c4997f635f9ddb567d09
Jenihacker <20cs064.jenison@sjec.ac.in>
2023-03-18 12:15:15 +0530

    updated project

--- static/script.js
+++ static/script.js
@@ -16,13 +16,33 @@ function checknum1() {
         return false;
     }
 }
+
+function checkopts() {
+    if(document.getElementById('payment-id').value === '') {
+        alert('Select an option from payment ID');
+        return false;
+    }
+    else if(/^[0-9]{10}$/.test(document.getElementById('mobile-number').value) === false) {
+        alert("Phone number should be 10 digit number");
+        return false;    
+    }
+    return true;
+}
 function checkamt() {
     if(/[0-9]+/.test(document.getElementById('age').value) === false) {
         alert("The age should be numerical");
         return false;
     }
-    else if(/[0-9]{10}/.test(document.getElementById('mobno').value) === false) {
+    else if(/^[0-9]{10}$/.test(document.getElementById('mobno').value) === false) {
         alert("Phone number should be 10 digit number");
+        return false;
+    }
+    else if(document.getElementById('pay-id').value === '') {
+        alert('Select an option from payment ID');
+        return false;
+    }
+    else if(document.getElementById('trainer-id').value === '') {
+        alert('Select an option from trainer ID');
         return false;
     }
     return true;
